4d1fed6166db643b9bc4f36af52228ce56cdd427
Sebastián González <brutalchrist@gmail.com>
2018-04-10 18:44:09 -0300

    Annotation helper

--- generators/helper.js
+++ generators/helper.js
@@ -3,6 +3,9 @@ const camelize = require('camelize');
 module.exports = (name, package) => {
     return `package ${package}.helpers;
 
+import earlgrey.annotations.Helper;
+
+@Helper()
 public class ${camelize(name)} {
 
 }
